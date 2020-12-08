bd658df506e9458ad3654115a6167d0518620e5e
Marcos Ramos <rms.mrcs@gmail.com>
2020-12-08 11:51:36 -0300

    Add support for custom enum prefix

--- lib/sift/schema/types/enum.ex
+++ lib/sift/schema/types/enum.ex
@@ -9,19 +9,28 @@ defmodule Sift.Schema.Types.Enum do
   @impl Type
   def parse(value, allowed_values)
 
+  def parse(value, {prefix, allowed_values}) do
+    do_parse(value, allowed_values, prefix)
+  end
+
   def parse(value, allowed_values) when is_atom(value) do
-    case value in allowed_values do
-      true -> {:ok, "$#{Atom.to_string(value)}"}
-      false -> {:error, "invalid value '#{value}'"}
-    end
+    do_parse(value, allowed_values, "$")
   end
 
   def parse(value, allowed_values) when is_binary(value) do
-    case value in allowed_values do
-      true -> {:ok, value}
-      false -> {:error, "invalid value '#{value}'"}
-    end
+    do_parse(value, allowed_values)
   end
 
   def parse(_value, _opts), do: {:error, "invalid value"}
+
+  def do_parse(value, allowed_values, prefix \\ "") do
+    case value in allowed_values do
+      true ->
+        value = if is_atom(value), do: Atom.to_string(value), else: value
+        {:ok, prefix <> value}
+
+      false ->
+        {:error, "invalid value '#{value}'"}
+    end
+  end
 end
